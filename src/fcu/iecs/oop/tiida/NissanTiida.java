2e991a1d5ff6d2a3a7d214ea7f3dccaf77e36d9b
User <User@DESKTOP-M9BMA8N>
2016-10-25 22:39:16 +0900

    新增pom.xml

--- src/fcu/iecs/oop/tiida/NissanTiida.java
+++ src/fcu/iecs/oop/tiida/NissanTiida.java
@@ -4,7 +4,7 @@ public class NissanTiida {
 	
 	public int num=1;
 	public void tiida(){
-		System.out.printf("²Ä%d¦¸©I¥s",num);
+		System.out.printf("Time %d",num);
 		for(int i=0;i<num;i++){
 			System.out.println("\n");
 			for(int j=0;j<num;j++){
